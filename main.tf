a14f15f6bc6ee079fc3e45573eba4d5fd9807d23
globaluser <globaluser@hotmail.com>
2026-04-01 18:24:24 +0530

    corrected bucket name

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1" 
 } 
 resource "aws_s3_bucket" "demo_bucket" { 
-  bucket = "my-terraform-cicd-demo-bucket-12345" 
+  bucket = "dinakar15-terraform-cicd-demo-bucket-20260401" 
 
   tags = { Name = "Terraform CICD Demo" 
   } 
